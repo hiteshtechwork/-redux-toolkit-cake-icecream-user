58c9993c046faeb1de7896636e01d4bca2f7e60d
hvrishabh <hitesh.verma@rishabhsoft.com>
2024-04-09 16:50:00 +0530

    first project for the redux-toolkit-cake-icecream-user

---
+++ features/icecream/icecreamSlice.js
@@ -0,0 +1,33 @@
+const { cakeActions } = require("../cake/cakeSlice");
+
+const createSlice = require("@reduxjs/toolkit").createSlice;
+
+const initialState = {
+  numOfIceCreams: 20,
+};
+
+const icecreamSlice = createSlice({
+  name: "icecream",
+  initialState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numOfIceCreams--;
+    },
+    restocked: (state, action) => {
+      state.numOfIceCreams += action.payload;
+    },
+  },
+  //   extraReducers: {
+  //     ["cake/ordered"]: (state) => {
+  //       state.numOfIceCreams--;
+  //     },
+  //   },
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numOfIceCreams--;
+    });
+  },
+});
+
+module.exports = icecreamSlice.reducer;
+module.exports.icecreamActions = icecreamSlice.actions;
